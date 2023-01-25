a0155b4957a18273afae6894b7fc869e332179fe
Sri Ritwik <56584349+sriritwik3@users.noreply.github.com>
2023-01-25 22:33:10 +0530

    Add files via upload

---
+++ models/userSchema.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: 'Your email is required',
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: 'Your password is required',
+        max: 100,
+    },
+});
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
